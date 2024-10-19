04b391bdb32ac0c6812620a35d1ea38e69015552
mahmoud9877 <mohmoudali9877@gmail.com>
2024-10-19 20:11:05 +0300

    cartUpdate

--- src/modules/cart/controller/cart.js
+++ src/modules/cart/controller/cart.js
@@ -3,7 +3,7 @@ import productModel from "../../../../DB/model/Product.Model.js";
 import cartModel from "../../../../DB/model/Cart.Model.js";
 
 export const getCart = asyncHandler(async (req, res, next) => {
-  const cart = await cartModel.findOne({ userId: req.user._id });
+  const cart = await cartModel.find({});
 });
 // Add to cart handler
 export const addToCart = asyncHandler(async (req, res, next) => {
